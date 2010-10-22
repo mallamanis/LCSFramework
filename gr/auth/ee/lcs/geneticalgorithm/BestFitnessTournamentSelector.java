08de303569a32b22b6853fdf0b2a97223f969ff6
mallamanis <mallamanis@gmail.com>
2010-10-22 21:36:55 +0300

    Implementation of the method

--- gr/auth/ee/lcs/geneticalgorithm/BestFitnessTournamentSelector.java
+++ gr/auth/ee/lcs/geneticalgorithm/BestFitnessTournamentSelector.java
@@ -29,15 +29,17 @@ public class BestFitnessTournamentSelector implements INaturalSelector {
 	public void select(int howManyToSelect, ClassifierSet fromPopulation,
 			ClassifierSet toPopulation) {
 		
-		// TODO Auto-generated method stub
+		for (int i=0;i<howManyToSelect;i++)
+			this.tournament(fromPopulation, toPopulation);
 
 	}
 	
 	/**
+	 * Runs a tournament in the fromPopulation with the size defined in tournamentSize (during construction)
+	 * The winner of the tournament is added to the toPopulation
+	 * @param fromPopulation the source population to run the tournament in
+	 * @param toPopulation the destination population of the tournament winners
 	 * 
-	 * @param fromPopulation
-	 * @param toPopulation
-	 * @return
 	 */
 	private void tournament( ClassifierSet fromPopulation, ClassifierSet toPopulation){
 		//Generate random participants
@@ -45,11 +47,34 @@ public class BestFitnessTournamentSelector implements INaturalSelector {
 		for (int i=0;i<tournamentSize;i++){
 			participants[i]=(int) Math.ceil((Math.random()*fromPopulation.totalNumerosity));
 		}
+		
 		//Sort by order
 		Arrays.sort(participants);
-		float bestFitness=0;
-		int bestParticipant=-1;
-		//TODO scan for best...
+		double bestFitness=0; //Best fitness found in tournament
+		int currentParticipantIndex=0; //the index in the participants array of the current tournament participant
+		int bestParticipant=-1; //The current best participant
+		int currentClassifierIndex=0; //the index of the actual classifier (counting numerosity)
+		int currentMacroclassifierIndex=0; //The index of the macroclassifier being used
+		//Run tournament
+		do{
+			currentClassifierIndex+=fromPopulation.getClassifierNumerosity(currentMacroclassifierIndex);
+			//Is the current macroclassifier the participant?
+			if (currentClassifierIndex>=participants[currentParticipantIndex]){
+				//Find best
+				if (fromPopulation.getClassifier(currentMacroclassifierIndex).fitness>bestFitness){
+					bestFitness=fromPopulation.getClassifier(currentMacroclassifierIndex).fitness;
+					bestParticipant=currentMacroclassifierIndex;
+				}
+					
+				currentParticipantIndex++;
+			}else{			
+				currentMacroclassifierIndex++;
+			}
+		}while(currentParticipantIndex<tournamentSize);
+		
+		//Add winner to set
+		toPopulation.addClassifier(fromPopulation.getClassifier(bestParticipant), 1);
+		
 	}
 
 }
