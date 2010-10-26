26594af4779d13f6cb9a45fc3d482de4e7b9057c
mallamanis <mallamanis@gmail.com>
2010-10-26 19:03:27 +0300

    implementation

--- gr/auth/ee/lcs/geneticalgorithm/NichedSteadyStateFitnessProportionalGeneticAlgorithm.java
+++ gr/auth/ee/lcs/geneticalgorithm/NichedSteadyStateFitnessProportionalGeneticAlgorithm.java
@@ -1,6 +1,8 @@
 package gr.auth.ee.lcs.geneticalgorithm;
 
+import gr.auth.ee.lcs.classifiers.Classifier;
 import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.classifiers.DummySizeControlStrategy;
 
 
 
@@ -11,14 +13,43 @@ import gr.auth.ee.lcs.classifiers.ClassifierSet;
  */
 public class NichedSteadyStateFitnessProportionalGeneticAlgorithm implements IGeneticAlgorithmStrategy {
 
-  public double subsumptionStrengthThreshold;
+  public INaturalSelector gaSelector;
 
-  public Integer timeForGAActivation;
-
+  public IBinaryGeneticOperator crossoverOp;
+  
+  public IUnaryGeneticOperator mutationOp;
+  
+  private int timestamp=0;
   @Override
+  /**
+   * Evovles a set
+   * If the set is empty an exception will be thrown
+   */
   public void evolveSet(ClassifierSet evolveSet, ClassifierSet population) {
-	// TODO Auto-generated method stub
+	 
+	  timestamp++;
+	  
+	for (int i=0;i<evolveSet.getNumberOfMacroclassifiers();i++){
+		evolveSet.getClassifier(i).experience++;
+		evolveSet.getClassifier(i).timestamp=timestamp;
+	}
+	  
+	ClassifierSet parents=new ClassifierSet(new DummySizeControlStrategy());
+	//Select parents
+	gaSelector.select(1, evolveSet, parents);
+	Classifier parentA=parents.getClassifier(0);
+	parents.deleteClassifier(0);
+	gaSelector.select(1, evolveSet, parents);
+	Classifier parentB=parents.getClassifier(0);
+	parents.deleteClassifier(0);
 	
+	//Reproduce
+	for (int i=0;i<2;i++){
+		//produce a child
+		Classifier child=crossoverOp.operate(parentA, parentB);
+		child=mutationOp.operate(child);
+		population.addClassifier(child,1);
+	}
   }
 
 }
