5082a4b2af8cd81200c7818a82ca392ca8efdba3
mallamanis <mallamanis@gmail.com>
2010-10-26 13:21:54 +0300

    implementation

--- gr/auth/ee/lcs/data/ASLCSUpdateAlgorithm.java
+++ gr/auth/ee/lcs/data/ASLCSUpdateAlgorithm.java
@@ -5,7 +5,17 @@ import gr.auth.ee.lcs.classifiers.Classifier;
 
 public class ASLCSUpdateAlgorithm extends AbstractSLCSUpdateAlgorithm {
 
+	public double n;
+	
   public void updateFitness(Classifier aClassifier, ClassifierSet correctSet) {
+	  GenericSLCSClassifierData data=((GenericSLCSClassifierData)aClassifier.updateData);
+	  if (correctSet.getClassifierNumerosity(aClassifier)>0) //aClassifier belongs to correctSet
+		  data.tp++;
+	  else
+		  data.fp++;
+	  
+	  aClassifier.fitness=Math.pow(data.tp/data.msa,n);
+	  
   }
 
 }
