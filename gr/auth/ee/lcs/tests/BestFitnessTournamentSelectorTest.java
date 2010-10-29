60516264ff7c5520d941ff80cd027bc5b3e91f46
mallamanis <mallamanis@gmail.com>
2010-10-29 23:46:05 +0300

    initial scaffolding

---
+++ gr/auth/ee/lcs/tests/BestFitnessTournamentSelectorTest.java
@@ -0,0 +1,19 @@
+package gr.auth.ee.lcs.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BestFitnessTournamentSelectorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTournament() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
