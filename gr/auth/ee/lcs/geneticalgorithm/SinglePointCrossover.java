b2c4af1c6fe92a346609b5a4e67ab9bf4307e730
mallamanis <mallamanis@gmail.com>
2010-10-30 00:40:04 +0300

    minor comment typos

--- gr/auth/ee/lcs/geneticalgorithm/SinglePointCrossover.java
+++ gr/auth/ee/lcs/geneticalgorithm/SinglePointCrossover.java
@@ -4,7 +4,7 @@ import gr.auth.ee.lcs.classifiers.Classifier;
 import gr.auth.ee.lcs.classifiers.ExtendedBitSet;
 
 /**
- *  A binary genetic operator that performs gene crossover at a sigle point
+ *  A binary genetic operator that performs gene crossover at a single point
  * @author Miltos Allamanis
  *
  */
@@ -28,10 +28,10 @@ public class SinglePointCrossover implements IBinaryGeneticOperator {
   
 	/**
 	 * A protected function that performs a single point crossover
-	 * @param chromosomeA the fisrt chromosome to crossover
+	 * @param chromosomeA the first chromosome to crossover
 	 * @param chromosomeB the second chromosome to crossover
 	 * @param position the position (bit) to perform the crossover
-	 * @return the new corssovered (child) chromosome
+	 * @return the new cross-overed (child) chromosome
 	 */
 	protected ExtendedBitSet performCrossover(ExtendedBitSet chromosomeA,ExtendedBitSet chromosomeB,int position){
 	  ExtendedBitSet child=(ExtendedBitSet) chromosomeA.clone();
