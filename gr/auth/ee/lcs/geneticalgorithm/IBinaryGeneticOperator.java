36c34b4706c39258f6ba73bb984c3d5355def2e6
mallamanis <mallamanis@gmail.com>
2010-10-30 00:51:41 +0300

    added comments

--- gr/auth/ee/lcs/geneticalgorithm/IBinaryGeneticOperator.java
+++ gr/auth/ee/lcs/geneticalgorithm/IBinaryGeneticOperator.java
@@ -2,8 +2,19 @@ package gr.auth.ee.lcs.geneticalgorithm;
 
 import gr.auth.ee.lcs.classifiers.Classifier;
 
+/**
+ * An interface for representing all binary operators on classifiers
+ * @author Miltos Allamanis
+ *
+ */
 public interface IBinaryGeneticOperator {
 
-  public Classifier operate(Classifier classifierA, Classifier classifierB);
+	/**
+	 * The operation of the operator
+	 * @param classifierA the first argument of the binary classifier
+	 * @param classifierB the second argument of the binary classifier
+	 * @return the result of the operator
+	 */
+	public Classifier operate(Classifier classifierA, Classifier classifierB);
 
 }
