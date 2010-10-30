6465f6266e253c49eeaaa307a9e0132a51e06f03
mallamanis <mallamanis@gmail.com>
2010-10-30 21:59:59 +0300

    modifications on the algorithm. Still buggy

--- gr/auth/ee/lcs/tests/SimpleBoolTest.java
+++ gr/auth/ee/lcs/tests/SimpleBoolTest.java
@@ -6,6 +6,7 @@ package gr.auth.ee.lcs.tests;
 import gr.auth.ee.lcs.LCSTrainTemplate;
 import gr.auth.ee.lcs.classifiers.ClassifierSet;
 import gr.auth.ee.lcs.classifiers.DummySizeControlStrategy;
+import gr.auth.ee.lcs.classifiers.FixedSizeSetWorstFitnessTournamentDeletion;
 import gr.auth.ee.lcs.classifiers.OverpopulationInverseFitnessProportionateDeletion;
 import gr.auth.ee.lcs.data.ClassifierTransformBridge;
 import gr.auth.ee.lcs.data.ASLCSUpdateAlgorithm;
@@ -30,18 +31,18 @@ public class SimpleBoolTest {
 	public static void main(String[] args) {
 		LCSTrainTemplate myExample=new LCSTrainTemplate();
 		myExample.ga=new NichedSteadyStateFitnessProportionalGeneticAlgorithm(
-				new WeightedRouletteSelector(),
+				new BestFitnessTournamentSelector(10,true),
 				new SinglePointCrossover(),
-				new UniformBitMutation(.04),50);
+				new UniformBitMutation(.04),15);
 		
 		ClassifierTransformBridge.instance=new SimpleBooleanRepresentation(.33,3);
 		UpdateAlgorithmFactoryAndStrategy.currentStrategy=new ASLCSUpdateAlgorithm(5);
 		
-		ClassifierSet rulePopulation=new ClassifierSet(new DummySizeControlStrategy());//new OverpopulationInverseFitnessProportionateDeletion(100));
+		ClassifierSet rulePopulation=new ClassifierSet(new FixedSizeSetWorstFitnessTournamentDeletion(27,5));
 		
 		int trainSet[][][]=new int[2][2][2];
 		fillSet(trainSet);
-		for (int rep=0;rep<1000;rep++)//Repeat 1000times
+		for (int rep=0;rep<100;rep++){//Repeat 100times
 			for (int a=0;a<2;a++)
 				for (int b=0;b<2;b++)
 					for (int c=0;c<2;c++){
@@ -50,10 +51,13 @@ public class SimpleBoolTest {
 						visionVector[1]=b;
 						visionVector[2]=a;
 						myExample.trainWithInstance(rulePopulation, visionVector, trainSet[a][b][c]);
+					
+			
+						System.out.println("Iteration "+rep + "Trained:"+a+b+c);
+						for (int i=0;i<rulePopulation.getNumberOfMacroclassifiers();i++){
+							System.out.println(rulePopulation.getClassifier(i).toString()+" fit:"+rulePopulation.getClassifier(i).fitness+" exp:"+rulePopulation.getClassifier(i).experience+" num:"+rulePopulation.getClassifierNumerosity(i));
+						}
 					}
-		
-		for (int i=0;i<rulePopulation.getNumberOfMacroclassifiers();i++){
-			System.out.println(rulePopulation.getClassifier(i).toString()+" "+rulePopulation.getClassifier(i).fitness+" "+rulePopulation.getClassifier(i).experience);
 		}
 		
 		
