cee906fee53ea6e087a510206f5196d70ec19297
mallamanis <mallamanis@gmail.com>
2010-12-07 12:04:25 +0200

    documentation additions

--- gr/auth/ee/lcs/data/ASLCSUpdateAlgorithm.java
+++ gr/auth/ee/lcs/data/ASLCSUpdateAlgorithm.java
@@ -3,14 +3,29 @@ package gr.auth.ee.lcs.data;
 import gr.auth.ee.lcs.classifiers.ClassifierSet;
 import gr.auth.ee.lcs.classifiers.Classifier;
 
+/**
+ * The update algorithm for the AS-LCS
+ * @author Miltos Allamanis
+ *
+ */
 public class ASLCSUpdateAlgorithm extends AbstractSLCSUpdateAlgorithm {
 
+	/**
+	 * The strictness factor for updating
+	 */
 	private double n;
 	
+	/**
+	 * Object's Constuctor
+	 * @param n the strictness factor ν used in updating
+	 */
 	public ASLCSUpdateAlgorithm(double n){
 		this.n=n;
 	}
 	
+	/**
+	 * implements @see gr.auth.ee.cs.data.AbstractSLCSUpdateAlgorithm
+	 */
   public void updateFitness(Classifier aClassifier, int numerosity, ClassifierSet correctSet) {
 	  GenericSLCSClassifierData data=((GenericSLCSClassifierData)aClassifier.updateData);
 	  if (correctSet.getClassifierNumerosity(aClassifier)>0) //aClassifier belongs to correctSet
