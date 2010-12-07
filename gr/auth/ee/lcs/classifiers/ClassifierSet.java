094de83570a116df016ca81312480ebc98630993
mallamanis <mallamanis@gmail.com>
2010-12-07 11:08:13 +0200

    Serialization & open/save methods implementation

--- gr/auth/ee/lcs/classifiers/ClassifierSet.java
+++ gr/auth/ee/lcs/classifiers/ClassifierSet.java
@@ -2,29 +2,44 @@ package gr.auth.ee.lcs.classifiers;
 
 import gr.auth.ee.lcs.data.ClassifierTransformBridge;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Vector;
 
 
 /** 
- *  implement abstractSet of Classifiers, counting numerosity
+ *  Implement set of Classifiers, counting numerosity fo classifiers
+ *  This object is serializabe
  *  @author Miltos Allamanis
  */
-public class ClassifierSet {
+public class ClassifierSet implements Serializable {
 
-  public int totalNumerosity=0;
+  /**
+	 * Serialization id for versioning
+	 */
+	private static final long serialVersionUID = 2664983888922912954L;
+
+public int totalNumerosity=0;
 
    /**
    * Macroclassifier
    */
   public Vector<Macroclassifier>  myMacroclassifier;
   /** 
-   *  An interface for a strategy on deleting classifiers from the set
+   *  An interface for a strategy on deleting classifiers from the set. 
+   *  This attribute is transient and therefore not serializable
    */
-  public ISizeControlStrategy myISizeControlStrategy;
+  transient public ISizeControlStrategy myISizeControlStrategy;
 
  
   /** 
-   *  Adds a classifier with the a given numerosity to the set. It checks whether the classifier already exists and increases its numerosity. It also checks for subsumption. It also updates the set's numerosity
+   *  Adds a classifier with the a given numerosity to the set.
+   *  It checks whether the classifier already exists and increases its numerosity.
+   *  It also checks for subsumption nad updates the set's numerosity
    */
   public void addClassifier(Classifier aClassifier, int numerosity) {
 	  //Add numerosity to the Set
@@ -54,14 +69,14 @@ public class ClassifierSet {
   }
 
   /** 
-   *  returns the set's total numerosity
+   *  returns the set's total numerosity (the total number of microclassifiers)
    */
   public int getTotalNumerosity() {
 	  return this.totalNumerosity;
   }
 
   /** 
-   *  returns a classifier's numerosity
+   *  returns a classifier's numerosity (the number of microclassifiers)
    */
   public int getClassifierNumerosity(Classifier aClassifier) {
 	  for (int i=0;i<myMacroclassifier.size();i++){
@@ -72,7 +87,7 @@ public class ClassifierSet {
   }
   
   /** 
-   *  removes a classifier from the set by either completely deleting it (if the classsifier's numerosity is 0) or by decreasing the numerosity
+   *  removes a micro-classifier from the set by either completely deleting it (if the classsifier's numerosity is 0) or by decreasing the numerosity
    */
   public void deleteClassifier(Classifier aClassifier) {
 	  		 
@@ -100,31 +115,30 @@ public class ClassifierSet {
 		  this.myMacroclassifier.remove(index);
   }
 
+  /**
+   * 
+   * @return the number of macroclassifiers in the set
+   */
   public int getNumberOfMacroclassifiers() {
 	  return this.myMacroclassifier.size();
   }
 
   /** 
-   *  returns the classifier with the specified index in the macroclasifier vector
+   *  @return the classifier with the specified index in the macroclassifiers vector
    */
   public Classifier getClassifier(int index) {
 	  return this.myMacroclassifier.elementAt(index).myClassifier;
   }
 
   /** 
-   *  returns true if the set is empty
+   *  @return true if the set is empty
    */
   public boolean isEmpty() {
 	  return this.myMacroclassifier.isEmpty();
   }
 
-  public void addFromClassifierSet(ClassifierSet set, int index) {
-	  //TODO: Implement if needed...
-	  
-  }
-
   /**
-   * The default constructor
+   * The default ClassifierSet constructor
    */
   public ClassifierSet(ISizeControlStrategy SizeControlStrategy) {
 	  this.myISizeControlStrategy=SizeControlStrategy;
@@ -139,6 +153,55 @@ public class ClassifierSet {
    */
   public int getClassifierNumerosity(int index){
 	  return this.myMacroclassifier.elementAt(index).numerosity;
+  }
+  
+  /**
+   * A static function to save the classifier set
+   * @param toSave the set to be saved
+   * @param filename the path to save the set
+   */
+  public static void saveClassifierSet(ClassifierSet toSave, String filename){
+	  FileOutputStream fos = null;
+	  ObjectOutputStream out = null;
+	  
+	  try{
+		  fos = new FileOutputStream(filename);
+		  out = new ObjectOutputStream(fos);
+		  out.writeObject(toSave);
+		  out.close();
+		  
+	  }catch(IOException ex){
+		  ex.printStackTrace();
+	  }
+	  
+  }
+  
+  /**
+   * Open a saved (and serialized) ClassifierSet
+   * @param path the path of the ClassifierSet to be opened
+   * @param SizeControlStrategy the ClassifierSet's
+   * @return the opened classifier set 
+   */
+  public static ClassifierSet openClassifierSet(String path, ISizeControlStrategy SizeControlStrategy){
+	  FileInputStream fis = null;
+	  ObjectInputStream in = null;
+	  ClassifierSet opened = null;
+	  
+	  try{
+		  fis = new FileInputStream(path);
+		  in = new ObjectInputStream(fis);
+		  
+		  opened=(ClassifierSet)in.readObject();
+		  opened.myISizeControlStrategy=SizeControlStrategy;
+		  
+		  in.close();		  
+	  }catch(IOException ex){
+		  ex.printStackTrace();
+	  }catch(ClassNotFoundException ex){
+		  ex.printStackTrace();
+	  }
+	  
+	  return opened;
   }
 
 }
