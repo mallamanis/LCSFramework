8aeae31763ea05516a82af6375093e85f04cf7b0
mallamanis <mallamanis@gmail.com>
2010-12-20 20:15:49 +0200

    New cached instance matching

--- gr/auth/ee/lcs/ArffTrainer.java
+++ gr/auth/ee/lcs/ArffTrainer.java
@@ -4,6 +4,7 @@
 package gr.auth.ee.lcs;
 
 import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.data.ClassifierTransformBridge;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,7 +17,7 @@ import weka.core.Instances;
  */
 public class ArffTrainer {
 
-	private double[][] instancesValues;
+	
 	private int[] advocatedActions;
 	
 	public void loadInstances(String filename) throws IOException{
@@ -26,13 +27,13 @@ public class ArffTrainer {
 		if (instances.classIndex()<0)
 			instances.setClassIndex(instances.numAttributes() - 1);
 		
-		instancesValues=new double[instances.numInstances()][instances.numAttributes()-1];
+		ClassifierTransformBridge.instances=new double[instances.numInstances()][instances.numAttributes()-1];
 		advocatedActions=new int[instances.numInstances()];
 		
 		//Load instances
 		for (int i=0;i<instances.numInstances();i++){
 			for (int j=0;j<instances.numAttributes()-1;j++){
-				instancesValues[i][j]=instances.get(i).value(j);
+				ClassifierTransformBridge.instances[i][j]=instances.get(i).value(j);
 			}
 			advocatedActions[i]=(int)instances.get(i).value(instances.classIndex());
 		}
@@ -42,30 +43,26 @@ public class ArffTrainer {
 	public void train(LCSTrainTemplate lcs,int iterations,ClassifierSet population){
 		int numInstances=advocatedActions.length;
 		for (int repetition=0;repetition<iterations;repetition++){//Iterate
-			int i=repetition % numInstances;
-			lcs.trainWithInstance(population, instancesValues[i], advocatedActions[i]);
-			
-	
-				/*System.out.println("Iteration "+repetition + "Trained:"+instancesValues[i].toString());
-				for (int l=0;l<population.getNumberOfMacroclassifiers();l++){
-					System.out.println(population.getClassifier(l).toString()+" fit:"+population.getClassifier(l).fitness+" exp:"+population.getClassifier(l).experience+" num:"+population.getClassifierNumerosity(l));
-					System.out.println("Predicted Payoff: "+((XCSClassifierData)(population.getClassifier(l).updateData)).predictedPayOff);
-				}*/
+			System.out.println("Iteration "+repetition);
+			for (int i=0;i<numInstances;i++)
+					lcs.trainWithInstance(population, i, advocatedActions[i]);
+			//if (repetition % 10==0) population.selfSubsume();
 		}	
+		//population.selfSubsume();
 	}
 	
 	public void selfEvaluate(ClassifierSet population){
 		LCSExploitTemplate eval=new LCSExploitTemplate();
 		int tp=0,fp=0;
-		for (int i=0;i<instancesValues.length;i++){ //for each instance
-			if (eval.classifiy(instancesValues[i], population)==advocatedActions[i])
+		for (int i=0;i<ClassifierTransformBridge.instances.length;i++){ //for each instance
+			if (eval.classifiy(ClassifierTransformBridge.instances[i], population)==advocatedActions[i])
 				tp++;
-			else if (eval.classifiy(instancesValues[i], population)!=-1)
+			else if (eval.classifiy(ClassifierTransformBridge.instances[i], population)!=-1)
 				fp++;
 		}
 		
 		double errorRate=((double)fp)/((double)(fp+tp));
-		System.out.println("tp:"+tp+" fp:"+fp+" errorRate:"+errorRate+" total instances:"+instancesValues.length);
+		System.out.println("tp:"+tp+" fp:"+fp+" errorRate:"+errorRate+" total instances:"+ClassifierTransformBridge.instances.length);
 		
 	}
 	
