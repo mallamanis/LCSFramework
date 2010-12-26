77f296a3fd477a8096978fcb8e6e4b830c1e1f2b
mallamanis <mallamanis@gmail.com>
2010-12-26 23:47:15 +0200

    new experiment

--- gr/auth/ee/lcs/tests/SimpleBoolTest.java
+++ gr/auth/ee/lcs/tests/SimpleBoolTest.java
@@ -15,8 +15,9 @@ import gr.auth.ee.lcs.data.SimpleBooleanRepresentation;
 import gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy;
 import gr.auth.ee.lcs.data.XCSClassifierData;
 import gr.auth.ee.lcs.data.XCSUpdateAlgorithm;
+import gr.auth.ee.lcs.data.XSLCSUpdateAlgorithm;
 import gr.auth.ee.lcs.geneticalgorithm.BestClassifierSelector;
-import gr.auth.ee.lcs.geneticalgorithm.BestFitnessTournamentSelector;
+import gr.auth.ee.lcs.geneticalgorithm.TournamentSelector;
 import gr.auth.ee.lcs.geneticalgorithm.SteadyStateGeneticAlgorithm;
 import gr.auth.ee.lcs.geneticalgorithm.SinglePointCrossover;
 import gr.auth.ee.lcs.geneticalgorithm.UniformBitMutation;
@@ -32,29 +33,32 @@ public class SimpleBoolTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LCSTrainTemplate myExample=new LCSTrainTemplate();
+		LCSTrainTemplate myExample=new LCSTrainTemplate(0);
 		myExample.ga=new SteadyStateGeneticAlgorithm(
-				new BestFitnessTournamentSelector(10,true),
-				new SinglePointCrossover(),
+				new TournamentSelector(10,true,UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLORATION),
+				new SinglePointCrossover(),1,
 				new UniformBitMutation(.04),15);
 		
 		ClassifierTransformBridge.instance=new SimpleBooleanRepresentation(.33,3);
 		UpdateAlgorithmFactoryAndStrategy.currentStrategy=new ASLCSUpdateAlgorithm(5);
 		//UpdateAlgorithmFactoryAndStrategy.currentStrategy=new XCSUpdateAlgorithm(.2,10,.01,.1,3);
 		
-		ClassifierSet rulePopulation=new ClassifierSet(new FixedSizeSetWorstFitnessDeletion(300,new BestFitnessTournamentSelector(50,false)));
+		ClassifierSet rulePopulation=new ClassifierSet(new FixedSizeSetWorstFitnessDeletion(300,
+				new TournamentSelector(50,false,UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_DELETION)));
 		
 		int trainSet[][][]=new int[2][2][2];
 		fillSet(trainSet);
-		for (int rep=0;rep<1000;rep++){//Iterate
+		ClassifierTransformBridge.instances=new double[8][3];
+		fillInstance();
+		
+		
+		for (int rep=0;rep<8000;rep++){//Iterate
 			for (int a=0;a<2;a++)
 				for (int b=0;b<2;b++)
 					for (int c=0;c<2;c++){
 						double visionVector[]=new double[3];
-						visionVector[0]=c;
-						visionVector[1]=b;
-						visionVector[2]=a;
-						myExample.trainWithInstance(rulePopulation, visionVector, trainSet[a][b][c]);
+						
+						myExample.trainWithInstance(rulePopulation, 4*a+2*b+c, trainSet[a][b][c]);
 					
 			
 						System.out.println("Iteration "+rep + "Trained:"+a+b+c);
@@ -73,12 +77,48 @@ public class SimpleBoolTest {
 		//Fill with (aANDb)OR(aXORc)
 		trainSet[0][0][0]=0;
 		trainSet[0][0][1]=1;
-		trainSet[0][1][0]=0;
-		trainSet[0][1][1]=1;
-		trainSet[1][0][0]=1;
-		trainSet[1][0][1]=0;
-		trainSet[1][1][0]=1;
-		trainSet[1][1][1]=1;
+		trainSet[0][1][0]=2;
+		trainSet[0][1][1]=2;
+		trainSet[1][0][0]=3;
+		trainSet[1][0][1]=3;
+		trainSet[1][1][0]=3;
+		trainSet[1][1][1]=3;
+	}
+	
+	private static void fillInstance(){
+		ClassifierTransformBridge.instances[0][0]=0;
+		ClassifierTransformBridge.instances[0][1]=0;
+		ClassifierTransformBridge.instances[0][2]=0;
+		
+		ClassifierTransformBridge.instances[1][0]=1;
+		ClassifierTransformBridge.instances[1][1]=0;
+		ClassifierTransformBridge.instances[1][2]=0;
+		
+		ClassifierTransformBridge.instances[2][0]=0;
+		ClassifierTransformBridge.instances[2][1]=1;
+		ClassifierTransformBridge.instances[2][2]=0;
+		
+		ClassifierTransformBridge.instances[3][0]=1;
+		ClassifierTransformBridge.instances[3][1]=1;
+		ClassifierTransformBridge.instances[3][2]=0;
+		
+		ClassifierTransformBridge.instances[4][0]=0;
+		ClassifierTransformBridge.instances[4][1]=0;
+		ClassifierTransformBridge.instances[4][2]=1;
+		
+		ClassifierTransformBridge.instances[5][0]=1;
+		ClassifierTransformBridge.instances[5][1]=0;
+		ClassifierTransformBridge.instances[5][2]=1;
+		
+		ClassifierTransformBridge.instances[6][0]=0;
+		ClassifierTransformBridge.instances[6][1]=1;
+		ClassifierTransformBridge.instances[6][2]=1;
+		
+		ClassifierTransformBridge.instances[7][0]=1;
+		ClassifierTransformBridge.instances[7][1]=1;
+		ClassifierTransformBridge.instances[7][2]=1;
+		
+		
 	}
 
 }
