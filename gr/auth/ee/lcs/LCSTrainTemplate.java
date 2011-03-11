0bb84fefc2713761e6041f44028bac27478e2b09
Miltiadis Allamanis <mallamanis@gmail.com>
2011-03-11 18:30:35 +0200

    import changes

--- gr/auth/ee/lcs/LCSTrainTemplate.java
+++ gr/auth/ee/lcs/LCSTrainTemplate.java
@@ -1,11 +1,8 @@
 package gr.auth.ee.lcs;
 
-import gr.auth.ee.lcs.classifiers.Classifier;
 import gr.auth.ee.lcs.classifiers.ClassifierSet;
 import gr.auth.ee.lcs.classifiers.Macroclassifier;
-import gr.auth.ee.lcs.data.ClassifierTransformBridge;
 import gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy;
-import gr.auth.ee.lcs.geneticalgorithm.IGeneticAlgorithmStrategy;
 
 /**
  * This is a template algorithm for training LCSs
