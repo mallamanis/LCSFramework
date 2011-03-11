85f51ac5ee2283638f0b91d995d2bf88736a90f4
Miltiadis Allamanis <mallamanis@gmail.com>
2011-03-11 18:29:59 +0200

    Changes to weighted roulette wheel, for mix/max and other changes

--- gr/auth/ee/lcs/geneticalgorithm/selectors/WeightedRouletteSelector.java
+++ gr/auth/ee/lcs/geneticalgorithm/selectors/WeightedRouletteSelector.java
@@ -6,30 +6,48 @@ import gr.auth.ee.lcs.geneticalgorithm.INaturalSelector;
 
 /**
  * A Natural Selection operator performing a weighted roulette wheel selection.
+ * This implementation contracts that all classifier have positive values of
+ * fitness. TODO: Throw exception otherwise?
  * 
  * @author Miltos Allamanis
- * @deprecated
+ * 
  */
-@Deprecated
+
 public class WeightedRouletteSelector implements INaturalSelector {
 
 	/**
-	 * The comparison mode
+	 * The comparison mode used for fitness selecting.
 	 */
 	private final int mode;
+
+	/**
+	 * Private variable for selecting maximum or minimum selection.
+	 */
+	private final boolean max;
 
 	/**
 	 * Constructor.
 	 * 
 	 * @param comparisonMode
 	 *            the comparison mode
+	 * @param max
+	 *            whether the selector selects min or max fitness (when max,
+	 *            max=true)
 	 */
-	WeightedRouletteSelector(final int comparisonMode) {
+	public WeightedRouletteSelector(final int comparisonMode, final boolean max) {
 		mode = comparisonMode;
+		this.max = max;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * gr.auth.ee.lcs.geneticalgorithm.INaturalSelector#select(gr.auth.ee.lcs
+	 * .classifiers.ClassifierSet)
+	 */
 	@Override
-	public int select(ClassifierSet fromPopulation) {
+	public final int select(final ClassifierSet fromPopulation) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -46,27 +64,30 @@ public class WeightedRouletteSelector implements INaturalSelector {
 	 *            added
 	 */
 	@Override
-	public void select(final int howManyToSelect,
+	public final void select(final int howManyToSelect,
 			final ClassifierSet fromPopulation, final ClassifierSet toPopulation) {
 		// Find total sum
 		double fitnessSum = 0;
 		for (int i = 0; i < fromPopulation.getNumberOfMacroclassifiers(); i++) {
-			fitnessSum += fromPopulation.getClassifierNumerosity(i)
+			final double fitnessValue = fromPopulation
+					.getClassifierNumerosity(i)
 					* fromPopulation.getClassifier(i).getComparisonValue(mode);
+			fitnessSum += max ? fitnessValue : 1 / (fitnessValue);
 		}
 
 		// Repeat roulette for howManyToSelect times
 		for (int i = 0; i < howManyToSelect; i++) {
 			// Roulette
-			double rand = Math.random() * fitnessSum;
+			final double rand = Math.random() * fitnessSum;
 			double tempSum = 0;
 			int selectedIndex = -1;
 			do {
 				selectedIndex++;
-				tempSum += fromPopulation
+				final double tempValue = fromPopulation
 						.getClassifierNumerosity(selectedIndex)
 						* fromPopulation.getClassifier(selectedIndex)
 								.getComparisonValue(mode);
+				tempSum += max ? tempValue : 1 / (tempValue);
 			} while (tempSum < rand);
 			// Add selectedIndex
 			toPopulation.addClassifier(
@@ -75,5 +96,4 @@ public class WeightedRouletteSelector implements INaturalSelector {
 		} // next roulette
 
 	}
-
 }
