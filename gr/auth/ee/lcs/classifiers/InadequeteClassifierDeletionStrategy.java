9f9a1562ff333b9fca6580b2a38bc663043aa5db
mallamanis <mallamanis@gmail.com>
2011-03-18 13:20:34 +0200

    create intermediate deletion strategy

---
+++ gr/auth/ee/lcs/classifiers/InadequeteClassifierDeletionStrategy.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.classifiers;
+
+/**
+ * @author miltiadis
+ * 
+ */
+public class InadequeteClassifierDeletionStrategy implements
+		IPopulationControlStrategy {
+
+	final int instanceSize;
+
+	public InadequeteClassifierDeletionStrategy(int instanceSize) {
+		this.instanceSize = instanceSize;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * gr.auth.ee.lcs.classifiers.IPopulationControlStrategy#controlPopulation
+	 * (gr.auth.ee.lcs.classifiers.ClassifierSet)
+	 */
+	@Override
+	public void controlPopulation(ClassifierSet aSet) {
+
+		final int setSize = aSet.getNumberOfMacroclassifiers();
+
+		for (int i = setSize - 1; i >= 0; i--) {
+			final Classifier aClassifier = aSet.getClassifier(i);
+			final boolean zeroCoverage = aClassifier.getCheckedInstances() >= instanceSize
+					&& aClassifier.getCoverage() == 0;
+			if (zeroCoverage)
+				aSet.deleteClassifier(i);
+		}
+
+	}
+
+}
