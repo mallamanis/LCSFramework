09aa281113c1d4cb0dbd2431a2469a9257cc3542
mallamanis <mallamanis@gmail.com>
2011-03-22 23:55:42 +0200

    moved final UCS impementation

---
+++ gr/auth/ee/lcs/impementations/UCS.java
@@ -0,0 +1,171 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.impementations;
+
+import java.io.IOException;
+
+import gr.auth.ee.lcs.ArffTrainer;
+import gr.auth.ee.lcs.LCSTrainTemplate;
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.classifiers.FixedSizeSetWorstFitnessDeletion;
+import gr.auth.ee.lcs.classifiers.PostProcessPopulationControl;
+import gr.auth.ee.lcs.classifiers.SortPopulationControl;
+import gr.auth.ee.lcs.data.ClassifierTransformBridge;
+import gr.auth.ee.lcs.data.IEvaluator;
+import gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy;
+import gr.auth.ee.lcs.data.representations.UnilabelRepresentation;
+import gr.auth.ee.lcs.data.updateAlgorithms.UCSUpdateAlgorithm;
+import gr.auth.ee.lcs.evaluators.BinaryAccuracyEvalutor;
+import gr.auth.ee.lcs.evaluators.BinaryAccuracySelfEvaluator;
+import gr.auth.ee.lcs.evaluators.ConfusionMatrixEvaluator;
+import gr.auth.ee.lcs.geneticalgorithm.IGeneticAlgorithmStrategy;
+import gr.auth.ee.lcs.geneticalgorithm.algorithms.SteadyStateGeneticAlgorithm;
+import gr.auth.ee.lcs.geneticalgorithm.operators.SinglePointCrossover;
+import gr.auth.ee.lcs.geneticalgorithm.operators.UniformBitMutation;
+import gr.auth.ee.lcs.geneticalgorithm.selectors.RouletteWheelSelector;
+
+/**
+ * A UCS Learning Classifier System.
+ * @author Miltos Allamanis
+ *
+ */
+public class UCS {
+	
+	/**
+	 * The input file used (.arff).
+	 */
+	private final String inputFile;
+	
+	/**
+	 * The number of full iterations to train the UCS.
+	 */
+	private final int iterations;
+	
+	/**
+	 * The size of the population to use.
+	 */
+	private final int populationSize;
+	
+	/**
+	 * The GA crossover rate.
+	 */
+	private final float CROSSOVER_RATE = (float) 0.8; 
+	
+	/**
+	 * The GA mutation rate.
+	 */
+	private final double MUTATION_RATE = (float) .04;
+	
+	/**
+	 * The GA activation rate.
+	 */
+	private final int THETA_GA = 100;
+	
+	/**
+	 * The frequency at which callbacks will be called for evaluation.
+	 */
+	private final int CALLBACK_RATE = 10;
+	
+	/**
+	 * The number of bits to use for representing continuous variables
+	 */
+	private final int PRECISION_BITS = 7;
+	
+	private final double UCS_ALPHA = .1; 
+	
+	private final int UCS_N = 10;
+	
+	private final double UCS_ACC0 = .99;
+	
+	private final double UCS_LEARNING_RATE = .1;
+	
+	private final int UCS_EXPERIENCE_THRESHOLD = 50;
+	
+	public UCS(String filename, int iterations, int populationSize) {
+		inputFile = filename;
+		this.iterations = iterations;
+		this.populationSize =  populationSize;
+	}
+	
+	public void run() throws IOException {
+		LCSTrainTemplate myExample = new LCSTrainTemplate(CALLBACK_RATE);
+		IGeneticAlgorithmStrategy ga = new SteadyStateGeneticAlgorithm(
+			new RouletteWheelSelector(
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLORATION,
+				true), new SinglePointCrossover(), CROSSOVER_RATE,
+				new UniformBitMutation(MUTATION_RATE), THETA_GA);
+
+		
+		UnilabelRepresentation rep = new UnilabelRepresentation(inputFile, PRECISION_BITS);
+		rep.setClassificationStrategy(rep.new VotingClassificationStrategy());
+		ClassifierTransformBridge.setInstance(rep);
+
+		UpdateAlgorithmFactoryAndStrategy.currentStrategy = new UCSUpdateAlgorithm(
+				UCS_ALPHA, UCS_N, UCS_ACC0, UCS_LEARNING_RATE, UCS_EXPERIENCE_THRESHOLD, 0.01, ga, THETA_GA);
+		
+		ClassifierSet rulePopulation = new ClassifierSet(
+				new FixedSizeSetWorstFitnessDeletion(
+						populationSize,new RouletteWheelSelector(
+								UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_DELETION,
+								true)));
+		
+		
+		ArffTrainer trainer = new ArffTrainer();
+		trainer.loadInstances(inputFile);
+		myExample.train(iterations, rulePopulation);
+
+		for (int i = 0; i < rulePopulation.getNumberOfMacroclassifiers(); i++) {
+			System.out
+					.println(rulePopulation.getClassifier(i).toString()
+							+ " fit:"
+							+ rulePopulation
+									.getClassifier(i)
+									.getComparisonValue(
+											UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION)
+							+ " exp:"
+							+ rulePopulation.getClassifier(i).experience
+							+ " num:"
+							+ rulePopulation.getClassifierNumerosity(i));
+		}
+		System.out.println("Post process...");
+		PostProcessPopulationControl postProcess = new PostProcessPopulationControl(
+				10, 0, .5,
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		SortPopulationControl sort = new SortPopulationControl(
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		postProcess.controlPopulation(rulePopulation);
+		sort.controlPopulation(rulePopulation);
+		for (int i = 0; i < rulePopulation.getNumberOfMacroclassifiers(); i++) {
+			System.out
+					.println(rulePopulation.getClassifier(i).toString()
+							+ " fit:"
+							+ rulePopulation
+									.getClassifier(i)
+									.getComparisonValue(
+											UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION)
+							+ " exp:"
+							+ rulePopulation.getClassifier(i).experience
+							+ " num:"
+							+ rulePopulation.getClassifierNumerosity(i)
+							+ "cov:"
+							+ rulePopulation.getClassifier(i).getCoverage());
+			System.out
+					.println(UpdateAlgorithmFactoryAndStrategy.currentStrategy
+							.getData((rulePopulation.getClassifier(i))));
+		}
+		// ClassifierSet.saveClassifierSet(rulePopulation, "set");
+
+		final IEvaluator eval = new BinaryAccuracySelfEvaluator(true, true);
+		eval.evaluateSet(rulePopulation);
+		ConfusionMatrixEvaluator conf = new ConfusionMatrixEvaluator(
+				rep.getLabelNames(), ClassifierTransformBridge.instances);
+		conf.evaluateSet(rulePopulation);
+
+		System.out.println("Evaluating on test set");
+		BinaryAccuracyEvalutor testEval = new BinaryAccuracyEvalutor(
+				trainer.testSet, true);
+		testEval.evaluateSet(rulePopulation);
+
+	}
+}
