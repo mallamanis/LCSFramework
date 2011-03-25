e32b1af6574a4cf139cd6bf6b616de7b7414c7aa
mallamanis <mallamanis@gmail.com>
2011-03-25 23:34:46 +0200

    initial implementation of logger

---
+++ gr/auth/ee/lcs/evaluators/FileLogger.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.evaluators;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.data.IEvaluator;
+
+/**
+ * An evaluator logging output to a file.
+ * @author Miltos Allamanis
+ *
+ */
+public class FileLogger implements IEvaluator {
+
+	/**
+	 * The filename where output is logged.
+	 */
+	private final String file;
+	
+	/**
+	 * The evaluator from which we log the output.
+	 */
+	private final IEvaluator actualEvaluator;
+	
+	/**
+	 * FileLogger constructor
+	 * @param filename the filename of the file where log will be output.
+	 * @param evaluator the evaluator which we are going to output.
+	 */
+	public FileLogger(final String filename,final IEvaluator evaluator) {
+		file = filename;
+		actualEvaluator = evaluator;
+		try {
+			FileWriter fstream = new FileWriter(file,false);
+			BufferedWriter buffer = new BufferedWriter(fstream);
+			buffer.write("");
+			buffer.flush();
+			buffer.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see gr.auth.ee.lcs.data.IEvaluator#evaluateSet(gr.auth.ee.lcs.classifiers.ClassifierSet)
+	 */
+	@Override
+	public double evaluateSet(ClassifierSet classifiers) {
+		final double evalResult = actualEvaluator.evaluateSet(classifiers);
+		try {
+			FileWriter fstream = new FileWriter(file,true);
+			BufferedWriter buffer = new BufferedWriter(fstream);
+			buffer.write(String.valueOf(evalResult)+"\n");
+			buffer.flush();
+			buffer.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+		return 0;
+	}
+
+}
