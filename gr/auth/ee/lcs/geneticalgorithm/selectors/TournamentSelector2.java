5c43aec61c12eb82bbbcfe82eadcef48e041cea7
mallamanis <mallamanis@gmail.com>
2011-03-29 20:04:22 +0300

    New Implementations

---
+++ gr/auth/ee/lcs/geneticalgorithm/selectors/TournamentSelector2.java
@@ -0,0 +1,157 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.geneticalgorithm.selectors;
+
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.classifiers.Macroclassifier;
+import gr.auth.ee.lcs.geneticalgorithm.INaturalSelector;
+
+import java.util.Arrays;
+
+/**
+ * A tournament selecting the best fitness classifier.
+ * 
+ * @author Miltos Allamanis
+ * @deprecated use TournamentSelector
+ * 
+ */
+public class TournamentSelector2 implements INaturalSelector {
+
+	/**
+	 * The size of the tournaments.
+	 */
+	private final int tournamentSize;
+
+	/**
+	 * The type of the tournaments.
+	 */
+	private final boolean max;
+
+	/**
+	 * The percentage of population size, used for tournament selection.
+	 */
+	private final double percentSize;
+
+	/**
+	 * The comparison mode for the tournaments.
+	 */
+	private final int mode;
+
+	public TournamentSelector2(final double sizeOfTournaments,
+			final boolean max, final int comparisonMode) {
+		this.tournamentSize = 0;
+		this.max = max;
+		this.mode = comparisonMode;
+		percentSize = sizeOfTournaments;
+	}
+
+	/**
+	 * The default constructor of the selector.
+	 * 
+	 * @param sizeOfTournaments
+	 *            the size of the tournament
+	 * @param max
+	 *            if true the we select the max fitness participant, else we
+	 *            select the min.
+	 * @param comparisonMode
+	 *            comparison mode @see
+	 *            gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy
+	 */
+	public TournamentSelector2(final int sizeOfTournaments, final boolean max,
+			final int comparisonMode) {
+		this.tournamentSize = sizeOfTournaments;
+		this.max = max;
+		this.mode = comparisonMode;
+		percentSize = 0;
+	}
+
+	@Override
+	public int select(ClassifierSet fromPopulation) {
+		int size;
+		if (tournamentSize == 0)
+			size = (int) Math.floor(fromPopulation.getTotalNumerosity()
+					* percentSize);
+		else
+			size = tournamentSize;
+
+		int[] participants = new int[size];
+		// Create random participants
+		for (int j = 0; j < participants.length; j++) {
+			participants[j] = (int) Math.floor((Math.random() * fromPopulation
+					.getTotalNumerosity()));
+		}
+		return this.tournament(fromPopulation, participants);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gr.auth.ee.lcs.geneticalgorithm.INaturalSelector#select(int,
+	 * gr.auth.ee.lcs.classifiers.ClassifierSet,
+	 * gr.auth.ee.lcs.classifiers.ClassifierSet)
+	 */
+	@Override
+	public final void select(final int howManyToSelect,
+			final ClassifierSet fromPopulation, final ClassifierSet toPopulation) {
+
+		for (int i = 0; i < howManyToSelect; i++) {
+
+			toPopulation.addClassifier(
+					new Macroclassifier(fromPopulation.getClassifier(this
+							.select(fromPopulation)), 1), false);
+
+		}
+
+	}
+
+	/**
+	 * Runs a tournament in the fromPopulation with the size defined in
+	 * tournamentSize (during construction) The winner of the tournament is
+	 * added to the toPopulation.
+	 * 
+	 * @param fromPopulation
+	 *            the source population to run the tournament in
+	 * @param participants
+	 *            the int[] of indexes of participants
+	 * @return the index of the tournament winner
+	 */
+	public final int tournament(final ClassifierSet fromPopulation,
+			final int[] participants) {
+
+		// Sort by order
+		Arrays.sort(participants);
+
+		//Construct cumulative numerosity
+		final int populationSize = fromPopulation.getNumberOfMacroclassifiers();
+		int[] cumulativeNum = new int[populationSize];
+		cumulativeNum[0] = fromPopulation.getClassifierNumerosity(0);
+		for (int i = 1; i < populationSize; i++)
+			cumulativeNum[i]= cumulativeNum[i-1]+fromPopulation.getClassifierNumerosity(i);
+
+		int bestMacroclassifierParticipant = 0;
+		double winnerFitness = fromPopulation.getClassifier(getMacroIndex(participants[0],cumulativeNum)).getComparisonValue(mode);
+		for (int i = 1; i < participants.length; i++) {
+			final int currentMacro = getMacroIndex(participants[i],cumulativeNum);
+			final double currentFitness = fromPopulation.getClassifier(currentMacro).getComparisonValue(mode);
+			if (((currentFitness - winnerFitness) * (max?1:-1)) > 0 ){
+				winnerFitness = currentFitness;
+				bestMacroclassifierParticipant = currentMacro;
+			}
+		}
+		return bestMacroclassifierParticipant;
+
+	}
+	
+	private int getMacroIndex(int microIndex, int[] cumulativeNumerosity) {
+		int macroIndex=0;
+		for (int i = 0; i < cumulativeNumerosity.length; i++){
+			if (cumulativeNumerosity[i] > microIndex)
+				break;
+			macroIndex++;
+		}
+		return macroIndex;
+	}
+
+}
---
+++ gr/auth/ee/lcs/impementations/DirectMlSSLCS.java
@@ -0,0 +1,239 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.impementations;
+
+import gr.auth.ee.lcs.ArffLoader;
+import gr.auth.ee.lcs.LCSTrainTemplate;
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.classifiers.populationcontrol.FixedSizeSetWorstFitnessDeletion;
+import gr.auth.ee.lcs.classifiers.populationcontrol.PostProcessPopulationControl;
+import gr.auth.ee.lcs.classifiers.populationcontrol.SortPopulationControl;
+import gr.auth.ee.lcs.data.ClassifierTransformBridge;
+import gr.auth.ee.lcs.data.IEvaluator;
+import gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy;
+import gr.auth.ee.lcs.data.representations.GenericMultiLabelRepresentation;
+import gr.auth.ee.lcs.data.representations.StrictMultiLabelRepresentation;
+import gr.auth.ee.lcs.data.representations.GenericMultiLabelRepresentation.VotingClassificationStrategy;
+import gr.auth.ee.lcs.data.updateAlgorithms.MlSSLCSUpdateAlgorithm;
+import gr.auth.ee.lcs.evaluators.AccuracyEvaluator;
+import gr.auth.ee.lcs.evaluators.ExactMatchEvalutor;
+import gr.auth.ee.lcs.evaluators.ExactMatchSelfEvaluator;
+import gr.auth.ee.lcs.evaluators.FileLogger;
+import gr.auth.ee.lcs.evaluators.HammingLossEvaluator;
+import gr.auth.ee.lcs.geneticalgorithm.IGeneticAlgorithmStrategy;
+import gr.auth.ee.lcs.geneticalgorithm.algorithms.SteadyStateGeneticAlgorithm;
+import gr.auth.ee.lcs.geneticalgorithm.operators.SinglePointCrossover;
+import gr.auth.ee.lcs.geneticalgorithm.operators.UniformBitMutation;
+import gr.auth.ee.lcs.geneticalgorithm.selectors.TournamentSelector;
+
+import java.io.IOException;
+
+/**
+ * A Direct ml-SS-LCS using the mlSS-LCS
+ * @author Miltos Allamanis
+ *
+ */
+public class DirectMlSSLCS {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		final String file = "/home/miltiadis/Desktop/datasets/mlTestbeds/mlidentity7.arff";
+		final int numOfLabels = 7;
+		final int iterations = 1000;
+		final int populationSize = 2000;
+		DirectMlSSLCS dmlsslcs = new DirectMlSSLCS(file, iterations,
+				populationSize, numOfLabels);
+		dmlsslcs.run();
+
+	}
+
+	/**
+	 * The input file used (.arff).
+	 */
+	private final String inputFile;
+
+	/**
+	 * The number of full iterations to train the UCS.
+	 */
+	private final int iterations;
+
+	/**
+	 * The size of the population to use.
+	 */
+	private final int populationSize;
+
+	/**
+	 * The GA crossover rate.
+	 */
+	private final float CROSSOVER_RATE = (float) 0.8;
+
+	/**
+	 * The GA mutation rate.
+	 */
+	private final double MUTATION_RATE = (float) .04;
+
+	/**
+	 * The GA activation rate.
+	 */
+	private final int THETA_GA = 100;
+
+	/**
+	 * The frequency at which callbacks will be called for evaluation.
+	 */
+	private final int CALLBACK_RATE = 10;
+
+	/**
+	 * The number of bits to use for representing continuous variables
+	 */
+	private final int PRECISION_BITS = 7;
+
+	/**
+	 * The UCS n power parameter.
+	 */
+	private final int SSLCS_PENALTY = 100;
+
+	/**
+	 * The accuracy threshold parameter.
+	 */
+	private final double SSLCS_REWARD = 1;
+
+	/**
+	 * The post-process experience threshold used.
+	 */
+	private final int POSTPROCESS_EXPERIENCE_THRESHOLD = 10;
+
+	/**
+	 * Coverage threshold for post processing.
+	 */
+	private final int POSTPROCESS_COVERAGE_THRESHOLD = 0;
+
+	/**
+	 * Post-process threshold for fitness;
+	 */
+	private final double POSTPROCESS_FITNESS_THRESHOLD = .01;
+
+	/**
+	 * The number of labels used at the dmlUCS.
+	 */
+	private final int numberOfLabels;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param filename
+	 *            the filename of the trainset
+	 * @param iterations
+	 *            the number of iterations to run
+	 * @param populationSize
+	 *            the size of the population to use
+	 * @param numOfLabels
+	 *            the number of labels in the problem
+	 * @param labelGeneralizationProbability
+	 *            the probability of generalizing a label (during coverage)
+	 */
+	public DirectMlSSLCS(final String filename, final int iterations,
+			final int populationSize, final int numOfLabels) {
+		inputFile = filename;
+		this.iterations = iterations;
+		this.populationSize = populationSize;
+		this.numberOfLabels = numOfLabels;		
+	}
+
+	/**
+	 * Run the SGmlUCS
+	 * 
+	 * @throws IOException
+	 */
+	public void run() throws IOException {
+		LCSTrainTemplate myExample = new LCSTrainTemplate(CALLBACK_RATE);
+		IGeneticAlgorithmStrategy ga = new SteadyStateGeneticAlgorithm(
+				new TournamentSelector(
+						50,
+						true,
+						UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLORATION),
+				new SinglePointCrossover(), CROSSOVER_RATE,
+				new UniformBitMutation(MUTATION_RATE), THETA_GA);
+
+		StrictMultiLabelRepresentation rep = new StrictMultiLabelRepresentation(
+				inputFile, PRECISION_BITS, numberOfLabels,
+				StrictMultiLabelRepresentation.EXACT_MATCH);
+		rep.setClassificationStrategy(rep.new VotingClassificationStrategy());
+		ClassifierTransformBridge.setInstance(rep);
+
+		UpdateAlgorithmFactoryAndStrategy.currentStrategy = new MlSSLCSUpdateAlgorithm(
+				SSLCS_REWARD, SSLCS_PENALTY, numberOfLabels, ga, 50, .99);
+
+		ClassifierSet rulePopulation = new ClassifierSet(
+				new FixedSizeSetWorstFitnessDeletion(
+						populationSize,
+						new TournamentSelector(
+								40,
+								true,
+								UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_DELETION)));
+
+		ArffLoader loader = new ArffLoader();
+		loader.loadInstances(inputFile, true);
+		final IEvaluator eval = new ExactMatchSelfEvaluator(true, true);
+		myExample.registerHook(new FileLogger(inputFile + "_resultDGMlSSLCS.txt",
+				eval));
+		myExample.train(iterations, rulePopulation);
+
+		for (int i = 0; i < rulePopulation.getNumberOfMacroclassifiers(); i++) {
+			System.out
+					.println(rulePopulation.getClassifier(i).toString()
+							+ " fit:"
+							+ rulePopulation
+									.getClassifier(i)
+									.getComparisonValue(
+											UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION)
+							+ " exp:"
+							+ rulePopulation.getClassifier(i).experience
+							+ " num:"
+							+ rulePopulation.getClassifierNumerosity(i));
+		}
+		System.out.println("Post process...");
+		PostProcessPopulationControl postProcess = new PostProcessPopulationControl(
+				POSTPROCESS_EXPERIENCE_THRESHOLD,
+				POSTPROCESS_COVERAGE_THRESHOLD, POSTPROCESS_FITNESS_THRESHOLD,
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		SortPopulationControl sort = new SortPopulationControl(
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		postProcess.controlPopulation(rulePopulation);
+		sort.controlPopulation(rulePopulation);
+		for (int i = 0; i < rulePopulation.getNumberOfMacroclassifiers(); i++) {
+			System.out
+					.println(rulePopulation.getClassifier(i).toString()
+							+ " fit:"
+							+ rulePopulation
+									.getClassifier(i)
+									.getComparisonValue(
+											UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION)
+							+ " exp:"
+							+ rulePopulation.getClassifier(i).experience
+							+ " num:"
+							+ rulePopulation.getClassifierNumerosity(i)
+							+ "cov:"
+							+ rulePopulation.getClassifier(i).getCoverage());
+			System.out
+					.println(UpdateAlgorithmFactoryAndStrategy.currentStrategy
+							.getData((rulePopulation.getClassifier(i))));
+		}
+		// ClassifierSet.saveClassifierSet(rulePopulation, "set");
+
+		eval.evaluateSet(rulePopulation);
+
+		System.out.println("Evaluating on test set");
+		ExactMatchEvalutor testEval = new ExactMatchEvalutor(loader.testSet,
+				true);
+		testEval.evaluateSet(rulePopulation);
+		HammingLossEvaluator hamEval = new HammingLossEvaluator(loader.testSet,
+				true, numberOfLabels);
+		hamEval.evaluateSet(rulePopulation);
+		AccuracyEvaluator accEval = new AccuracyEvaluator(loader.testSet, true);
+		accEval.evaluateSet(rulePopulation);
+
+	}
+}
