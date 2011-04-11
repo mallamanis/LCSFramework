015eece67b45e7773a4645d71a6c119375cfb4dc
mallamanis <mallamanis@gmail.com>
2011-04-11 13:28:57 +0300

    initial implementation

---
+++ gr/auth/ee/lcs/impementations/BinaryRelevanceUCS.java
@@ -0,0 +1,233 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.impementations;
+
+import gr.auth.ee.lcs.ArffLoader;
+import gr.auth.ee.lcs.LCSTrainTemplate;
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.classifiers.populationcontrol.FixedSizeSetWorstFitnessDeletion;
+import gr.auth.ee.lcs.classifiers.populationcontrol.PostProcessPopulationControl;
+import gr.auth.ee.lcs.classifiers.populationcontrol.SortPopulationControl;
+import gr.auth.ee.lcs.data.ClassifierTransformBridge;
+import gr.auth.ee.lcs.data.IEvaluator;
+import gr.auth.ee.lcs.data.UpdateAlgorithmFactoryAndStrategy;
+import gr.auth.ee.lcs.data.representations.GenericMultiLabelRepresentation;
+import gr.auth.ee.lcs.data.updateAlgorithms.UCSUpdateAlgorithm;
+import gr.auth.ee.lcs.evaluators.AccuracyEvaluator;
+import gr.auth.ee.lcs.evaluators.ExactMatchEvalutor;
+import gr.auth.ee.lcs.evaluators.ExactMatchSelfEvaluator;
+import gr.auth.ee.lcs.evaluators.FileLogger;
+import gr.auth.ee.lcs.evaluators.HammingLossEvaluator;
+import gr.auth.ee.lcs.geneticalgorithm.IGeneticAlgorithmStrategy;
+import gr.auth.ee.lcs.geneticalgorithm.algorithms.SteadyStateGeneticAlgorithm;
+import gr.auth.ee.lcs.geneticalgorithm.operators.SinglePointCrossover;
+import gr.auth.ee.lcs.geneticalgorithm.operators.UniformBitMutation;
+import gr.auth.ee.lcs.geneticalgorithm.selectors.RouletteWheelSelector;
+
+import java.io.IOException;
+
+/**
+ * A Binary Relevance Problem Transformation UCS.
+ * 
+ * @author Miltos Allamanis
+ * 
+ */
+public class BinaryRelevanceUCS {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		final String file = "/home/miltiadis/Desktop/datasets/genbase.arff";
+		final int numOfLabels = 27;
+		final int iterations = 500;
+		final int populationSize = 1000;
+		BinaryRelevanceUCS dmlucs = new BinaryRelevanceUCS(file, iterations,
+				populationSize, numOfLabels);
+		dmlucs.run();
+
+	}
+
+	/**
+	 * The input file used (.arff).
+	 */
+	private final String inputFile;
+
+	/**
+	 * The number of full iterations to train the UCS.
+	 */
+	private final int iterations;
+
+	/**
+	 * The size of the population to use.
+	 */
+	private final int populationSize;
+
+	/**
+	 * The GA crossover rate.
+	 */
+	private final float CROSSOVER_RATE = (float) 0.8;
+
+	/**
+	 * The GA mutation rate.
+	 */
+	private final double MUTATION_RATE = (float) .04;
+
+	/**
+	 * The GA activation rate.
+	 */
+	private final int THETA_GA = 300;
+
+	/**
+	 * The frequency at which callbacks will be called for evaluation.
+	 */
+	private final int CALLBACK_RATE = 100;
+
+	/**
+	 * The number of bits to use for representing continuous variables
+	 */
+	private final int PRECISION_BITS = 7;
+
+	/**
+	 * The UCS alpha parameter.
+	 */
+	private final double UCS_ALPHA = .1;
+
+	/**
+	 * The UCS n power parameter.
+	 */
+	private final int UCS_N = 10;
+
+	/**
+	 * The accuracy threshold parameter.
+	 */
+	private final double UCS_ACC0 = .99;
+
+	/**
+	 * The learning rate (beta) parameter.
+	 */
+	private final double UCS_LEARNING_RATE = .1;
+
+	/**
+	 * The UCS experience threshold.
+	 */
+	private final int UCS_EXPERIENCE_THRESHOLD = 10;
+
+	/**
+	 * The post-process experince threshold used.
+	 */
+	private final int POSTPROCESS_EXPERIENCE_THRESHOLD = 10;
+
+	/**
+	 * Coverage threshold for post processing.
+	 */
+	private final int POSTPROCESS_COVERAGE_THRESHOLD = 0;
+
+	/**
+	 * Post-process threshold for fitness;
+	 */
+	private final double POSTPROCESS_FITNESS_THRESHOLD = .1;
+
+	/**
+	 * The number of labels used at the dmlUCS.
+	 */
+	private final int numberOfLabels;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param filename
+	 *            the filename of the UCS
+	 * @param iterations
+	 *            the number of iterations to run
+	 * @param populationSize
+	 *            the size of the population to use
+	 * @param numOfLabels
+	 *            the number of labels in the problem
+	 */
+	public BinaryRelevanceUCS(final String filename, final int iterations,
+			final int populationSize, final int numOfLabels) {
+		inputFile = filename;
+		this.iterations = iterations;
+		this.populationSize = populationSize;
+		this.numberOfLabels = numOfLabels;
+	}
+
+	/**
+	 * Runs the Direct-ML-UCS.
+	 * 
+	 * @throws IOException
+	 */
+	public void run() throws IOException {
+		LCSTrainTemplate myExample = new LCSTrainTemplate(CALLBACK_RATE);
+		IGeneticAlgorithmStrategy ga = new SteadyStateGeneticAlgorithm(
+				new RouletteWheelSelector(
+						UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLORATION,
+						true), new SinglePointCrossover(), CROSSOVER_RATE,
+				new UniformBitMutation(MUTATION_RATE), THETA_GA);
+
+		GenericMultiLabelRepresentation rep = new GenericMultiLabelRepresentation(
+				inputFile, PRECISION_BITS, numberOfLabels,
+				GenericMultiLabelRepresentation.HAMMING_LOSS, 0);
+		rep.setClassificationStrategy(rep.new VotingClassificationStrategy());
+		ClassifierTransformBridge.setInstance(rep);
+
+		UpdateAlgorithmFactoryAndStrategy.currentStrategy = new UCSUpdateAlgorithm(
+				UCS_ALPHA, UCS_N, UCS_ACC0, UCS_LEARNING_RATE,
+				UCS_EXPERIENCE_THRESHOLD, 0.01, ga, THETA_GA, 1);
+
+		ClassifierSet rulePopulation = new ClassifierSet(null);
+
+		ArffLoader loader = new ArffLoader();
+		loader.loadInstances(inputFile, true);
+		final IEvaluator eval = new ExactMatchSelfEvaluator(true, true);
+		myExample.registerHook(new FileLogger(inputFile + "_result.txt", eval));
+
+		for (int i = 0; i < numberOfLabels; i++) {
+			System.out.println("Training Binary Classifier for label " + i);
+			rep.activateLabel(i);
+			ClassifierSet brpopulation = new ClassifierSet(
+					new FixedSizeSetWorstFitnessDeletion(
+							populationSize,
+							new RouletteWheelSelector(
+									UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_DELETION,
+									true)));
+			myExample.train(iterations, brpopulation);
+			rep.reinforceDeactivatedLabels(brpopulation);
+			rulePopulation.merge(brpopulation);
+		}
+		rep.activateAllLabels();
+
+		System.out.println("Post process...");
+		PostProcessPopulationControl postProcess = new PostProcessPopulationControl(
+				POSTPROCESS_EXPERIENCE_THRESHOLD,
+				POSTPROCESS_COVERAGE_THRESHOLD, POSTPROCESS_FITNESS_THRESHOLD,
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		SortPopulationControl sort = new SortPopulationControl(
+				UpdateAlgorithmFactoryAndStrategy.COMPARISON_MODE_EXPLOITATION);
+		postProcess.controlPopulation(rulePopulation);
+		sort.controlPopulation(rulePopulation);
+		rulePopulation.print();
+		// ClassifierSet.saveClassifierSet(rulePopulation, "set");
+
+		eval.evaluateSet(rulePopulation);
+
+		System.out.println("Evaluating on test set");
+		ExactMatchEvalutor testEval = new ExactMatchEvalutor(loader.testSet,
+				true);
+		testEval.evaluateSet(rulePopulation);
+		HammingLossEvaluator hamEval = new HammingLossEvaluator(loader.testSet,
+				true, numberOfLabels);
+		hamEval.evaluateSet(rulePopulation);
+		AccuracyEvaluator accEval = new AccuracyEvaluator(loader.testSet, true);
+		accEval.evaluateSet(rulePopulation);
+		rep.setClassificationStrategy(rep.new VotingClassificationStrategy());
+		System.out.println("Evaluating on test set (voting)");
+		testEval.evaluateSet(rulePopulation);
+		hamEval.evaluateSet(rulePopulation);
+		accEval.evaluateSet(rulePopulation);
+
+	}
+
+}
