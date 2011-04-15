c40ceb813285517b81687290d33cb493c40a8a72
mallamanis <mallamanis@gmail.com>
2011-04-15 13:09:56 +0300

    Created single label evaluators

---
+++ gr/auth/ee/lcs/evaluators/AllSingleLabelEvaluator.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.evaluators;
+
+import weka.core.Instances;
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.data.IEvaluator;
+
+/**
+ * All label single evaluator. The class acts as a wrapper against single label evaluator.
+ * @author Miltos Allamanis
+ *
+ */
+public class AllSingleLabelEvaluator implements IEvaluator {
+	
+	/**
+	 * The set of instances to evaluate on.
+	 */
+	private final Instances instanceSet;
+	
+	/**
+	 * Single label evaluators.
+	 */
+	private final SingleLabelEvaluator[] evaluators;
+	
+	/**
+	 * Boolean indicating if we are going to print results 
+	 */
+	private final boolean print;
+
+	/**
+	 * 
+	 * @param evaluateSet
+	 * @param numberOfLabels
+	 * @param printResults
+	 */
+	public AllSingleLabelEvaluator(Instances evaluateSet, int numberOfLabels, boolean printResults) {
+		instanceSet = evaluateSet;
+		print = printResults;
+		evaluators = new SingleLabelEvaluator[numberOfLabels];
+		for (int i = 0; i < numberOfLabels; i++) 
+			evaluators[i] = new SingleLabelEvaluator(i, evaluateSet);
+	}
+	
+	/* (non-Javadoc)
+	 * @see gr.auth.ee.lcs.data.IEvaluator#evaluateSet(gr.auth.ee.lcs.classifiers.ClassifierSet)
+	 */
+	@Override
+	public double evaluateSet(ClassifierSet classifiers) {
+		double sum = 0;
+		for (int i = 0; i < evaluators.length; i++) {
+			final double result = evaluators[i].evaluateSet(classifiers);
+			if (print) {
+				System.out.println("Label "+i+" exact match:"+result);
+			}
+			sum += result;
+		}
+		return sum / ((double) evaluators.length);
+	}
+
+}
