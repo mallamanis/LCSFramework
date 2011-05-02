e5435bbdd35ba6f732309dd4096443a8b136a63f
mallamanis <mallamanis@gmail.com>
2011-05-02 17:30:55 +0300

    best classifier now takes into consideration fitness and numerosity

--- gr/auth/ee/lcs/geneticalgorithm/selectors/BestClassifierSelector.java
+++ gr/auth/ee/lcs/geneticalgorithm/selectors/BestClassifierSelector.java
@@ -74,7 +74,7 @@ public final class BestClassifierSelector implements INaturalSelector {
 		final int popSize = fromPopulation.getNumberOfMacroclassifiers();
 		for (int i = 0; i < popSize; i++) {
 			final double temp = fromPopulation.getClassifier(i)
-					.getComparisonValue(mode);
+					.getComparisonValue(mode) * fromPopulation.getClassifierNumerosity(i);
 			if ((max ? 1. : -1.) * (temp - bestFitness) > 0) {
 				bestFitness = temp;
 				bestIndex = i;
