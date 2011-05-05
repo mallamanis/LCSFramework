f311862c6403341055e6b0d45c71d4c43c498a79
mallamanis <mallamanis@gmail.com>
2011-05-06 00:19:09 +0300

    Implemented property loader

---
+++ gr/auth/ee/lcs/utilities/SettingsLoader.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Load parameters from a file
+ * 
+ * @author Miltiadis Allamanis
+ *
+ */
+public class SettingsLoader {
+	
+	private final Properties lcsProperties;
+	
+	private SettingsLoader() throws IOException {
+		lcsProperties = new Properties();
+		loadProperties("defaultLcs.properties");		
+	}
+	
+	private SettingsLoader(String file) throws IOException {
+		lcsProperties = new Properties();
+		loadProperties(file);			
+	}
+	
+	private void loadProperties(String filename) throws IOException{
+		final FileInputStream input = new FileInputStream("defaultLcs.properties");
+		lcsProperties.load(input);
+	}
+	
+	private double getNumericProperty(String propertyName, double defaultValue) {
+		try {
+			return Double.parseDouble(lcsProperties.getProperty(propertyName,Double.toString(defaultValue)));
+		} catch( Exception ex) {
+			return defaultValue;
+		}
+	}
+	
+	public static double getNumericSetting(String propertyName, double defaultValue) {
+		return instance.getNumericProperty(propertyName, defaultValue);
+	}
+	
+	private String getStringProperty(String propertyName, String defaultValue) {
+		return lcsProperties.getProperty(propertyName,defaultValue);
+	}
+	
+	public static String getStringSetting(String propertyName, String defaultValue) {
+		return instance.getStringProperty(propertyName, defaultValue);
+	}
+	
+	private static SettingsLoader instance;
+	
+	public static void loadSettings() throws IOException {
+		instance = new SettingsLoader();
+	}
+	
+	public static void loadSettings(String filename) throws IOException {
+		instance = new SettingsLoader(filename);
+	}
+	
+	
+}
