5fd5fe0b0ca51a560294feba08ea803c73c321c1
Miltiadis Allamanis <mallamanis@gmail.com>
2011-05-26 21:01:10 +0300

    renamed & created

---
+++ gr/auth/ee/lcs/FoldEvaluator.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs;
+
+import gr.auth.ee.lcs.utilities.InstanceToDoubleConverter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+import weka.core.Instances;
+
+/**
+ * n-fold evaluator
+ * 
+ * @author Miltiadis Allamanis
+ * 
+ */
+public class FoldEvaluator {
+
+	private final int numOfFolds;
+
+	private final AbstractLearningClassifierSystem prototype;
+
+	private final Instances instances;
+	private Instances trainSet;
+	private Instances testSet;
+
+	private double[][] evals;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param folds
+	 *            the number of folds
+	 * @param myLcs
+	 *            the LCS instance to be evaluated
+	 * @param filename
+	 *            the filename containing the instances
+	 * @throws IOException
+	 *             if the file is not found.
+	 */
+	public FoldEvaluator(int folds, AbstractLearningClassifierSystem myLcs,
+			final String filename) throws IOException {
+		numOfFolds = folds;
+		prototype = myLcs;
+		final FileReader reader = new FileReader(filename);
+		instances = new Instances(reader);
+
+		instances.randomize(new Random());
+
+	}
+
+	public void evaluate() {
+		for (int i = 0; i < numOfFolds; i++) {
+			AbstractLearningClassifierSystem foldLCS = prototype.createNew();
+			System.out.println("Training Fold " + i);
+			loadFold(i, foldLCS);
+			foldLCS.train();
+			// Gather results...
+			double[] results = foldLCS.getEvaluations(testSet);
+			gatherResults(results, i);
+		}
+
+		double[] means = calcMean(this.evals);
+		// print results
+		printEvals(means);
+	}
+
+	private double[] calcMean(double[][] results) {
+		double[] means = new double[results[0].length];
+		for (int i = 0; i < means.length; i++) {
+			double sum = 0;
+			for (int j = 0; j < numOfFolds; j++) {
+				sum += results[j][i];
+			}
+			means[i] = (sum) / (numOfFolds);
+		}
+		return means;
+	}
+
+	private void gatherResults(double[] results, int fold) {
+		if (evals == null) {
+			evals = new double[numOfFolds][results.length];
+		}
+
+		evals[fold] = results;
+
+	}
+
+	private void loadFold(int foldNumber, AbstractLearningClassifierSystem lcs) {
+
+		trainSet = instances.trainCV(numOfFolds, foldNumber);
+		lcs.instances = InstanceToDoubleConverter.convert(trainSet);
+		testSet = instances.testCV(numOfFolds, foldNumber);
+	}
+
+	private void printEvals(double[] means) {
+		final String[] names = prototype.getEvaluationNames();
+
+		for (int i = 0; i < means.length; i++) {
+			System.out.println(names[i] + ": " + means[i]);
+		}
+	}
+}
---
+++ gr/auth/ee/lcs/evaluators/AccuracyRecallEvaluator.java
@@ -0,0 +1,175 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.evaluators;
+
+import gr.auth.ee.lcs.AbstractLearningClassifierSystem;
+import gr.auth.ee.lcs.classifiers.ClassifierSet;
+import gr.auth.ee.lcs.data.ClassifierTransformBridge;
+import gr.auth.ee.lcs.data.IEvaluator;
+import gr.auth.ee.lcs.utilities.InstanceToDoubleConverter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import weka.core.Instances;
+
+/**
+ * An evaluator to evaluate on the accuracy of the classifiers.
+ * 
+ * @author Miltos Allamanis
+ * 
+ */
+public class AccuracyRecallEvaluator implements IEvaluator {
+
+	/**
+	 * Accuracy.
+	 */
+	public final static int TYPE_ACCURACY = 0;
+
+	/**
+	 * Recall.
+	 */
+	public final static int TYPE_RECALL = 1;
+
+	private final int currentType;
+
+	/**
+	 * The set of instances to evaluate on.
+	 */
+	private final double[][] instances;
+
+	/**
+	 * A boolean indicating if the evaluator is going to print the results.
+	 */
+	private final boolean printResults;
+
+	/**
+	 * The LCS instance being used.
+	 */
+	private final AbstractLearningClassifierSystem myLcs;
+
+	/**
+	 * A constructor using only instances.
+	 * 
+	 * @param instances
+	 *            the instance double[][] array
+	 * @param print
+	 *            true to print results to stdout
+	 * @param lcs
+	 *            the LCS instance used
+	 */
+	public AccuracyRecallEvaluator(final double[][] instances,
+			final boolean print, final AbstractLearningClassifierSystem lcs,
+			final int type) {
+		this.printResults = print;
+		this.instances = instances;
+		myLcs = lcs;
+		currentType = type;
+	}
+
+	/**
+	 * Constructor for creating evaluator with a Weka instance set.
+	 * 
+	 * @param instances
+	 *            the instances to be used
+	 * @param print
+	 *            true to print results to stdout
+	 * @param lcs
+	 *            the LCS instance used
+	 */
+	public AccuracyRecallEvaluator(final Instances instances,
+			final boolean print, final AbstractLearningClassifierSystem lcs,
+			final int type) {
+		this.instances = InstanceToDoubleConverter.convert(instances);
+		printResults = print;
+		myLcs = lcs;
+		currentType = type;
+	}
+
+	/**
+	 * Constructor for creating evaluator with .arff file.
+	 * 
+	 * @param arffFileName
+	 *            the arff file
+	 * @param print
+	 *            true to print output to stdout
+	 * @param lcs
+	 *            the LCS instance used
+	 * @param type
+	 *            classification type
+	 * @throws IOException
+	 *             when file is not found
+	 */
+	public AccuracyRecallEvaluator(final String arffFileName,
+			final boolean print, final AbstractLearningClassifierSystem lcs,
+			final int type) throws IOException {
+		printResults = print;
+		final FileReader reader = new FileReader(arffFileName);
+		this.instances = InstanceToDoubleConverter
+				.convert(new Instances(reader));
+		myLcs = lcs;
+		currentType = type;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * gr.auth.ee.lcs.data.IEvaluator#evaluateSet(gr.auth.ee.lcs.classifiers
+	 * .ClassifierSet)
+	 */
+	@Override
+	public final double evaluateSet(final ClassifierSet classifiers) {
+		final ClassifierTransformBridge bridge = myLcs
+				.getClassifierTransformBridge();
+
+		double sumOfAccuracies = 0;
+		double sumOfRecall = 0;
+		for (int i = 0; i < instances.length; i++) {
+			int unionOfLabels = 0;
+			int intersectionOfLabels = 0;
+
+			final int[] classes = bridge.classify(classifiers, instances[i]);
+			final int[] classification = bridge
+					.getDataInstanceLabels(instances[i]);
+
+			// Find symmetric differences
+			Arrays.sort(classes);
+			Arrays.sort(classification);
+			for (int j = 0; j < classes.length; j++) {
+				if (Arrays.binarySearch(classification, classes[j]) < 0) {
+					unionOfLabels++;
+				} else {
+					intersectionOfLabels++;
+					unionOfLabels++;
+				}
+			}
+			for (int j = 0; j < classification.length; j++) {
+				if (Arrays.binarySearch(classes, classification[j]) < 0)
+					unionOfLabels++;
+			}
+			final double instanceAccuracy = ((double) intersectionOfLabels)
+					/ ((double) unionOfLabels);
+			sumOfAccuracies += Double.isNaN(instanceAccuracy) ? 0
+					: instanceAccuracy;
+
+			final double instanceRecall = ((double) intersectionOfLabels)
+					/ ((double) classification.length);
+			sumOfRecall += Double.isNaN(instanceRecall) ? 0 : instanceRecall;
+		}
+		final double accuracy = sumOfAccuracies / (instances.length);
+		final double recall = sumOfRecall / (instances.length);
+
+		if (printResults) {
+			System.out.println("Accuracy: " + accuracy);
+			System.out.println("Recall: " + recall);
+		}
+		if (currentType == TYPE_ACCURACY)
+			return accuracy;
+		else
+			return recall;
+	}
+
+}
