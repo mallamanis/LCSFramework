36392d3a74b153c874e663d6455ebcc2e5cb88d6
mallamanis <mallamanis@gmail.com>
2011-07-28 19:25:51 +0300

    initial file

---
+++ src/gr/auth/ee/lcs/meta/BaggedEnsemble.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package gr.auth.ee.lcs.meta;
+
+import weka.core.Instances;
+import gr.auth.ee.lcs.AbstractLearningClassifierSystem;
+
+/**
+ * @author miltiadis
+ * 
+ */
+public class BaggedEnsemble extends AbstractLearningClassifierSystem {
+
+	@Override
+	public int[] classifyInstance(double[] instance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gr.auth.ee.lcs.AbstractLearningClassifierSystem#createNew()
+	 */
+	@Override
+	public AbstractLearningClassifierSystem createNew() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gr.auth.ee.lcs.AbstractLearningClassifierSystem#getEvaluationNames()
+	 */
+	@Override
+	public String[] getEvaluationNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * gr.auth.ee.lcs.AbstractLearningClassifierSystem#getEvaluations(weka.core
+	 * .Instances)
+	 */
+	@Override
+	public double[] getEvaluations(Instances testSet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gr.auth.ee.lcs.AbstractLearningClassifierSystem#train()
+	 */
+	@Override
+	public void train() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
