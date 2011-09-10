6e4c8001ac78c621543d6ef09a37e8055245e5db
Miltiadis Allamanis <mallamanis@gmail.com>
2011-09-10 20:07:31 +0300

    Fixed Accuracy/Recall test

--- test/gr/auth/ee/lcs/evaluators/RecallEvaluatorTest.java
+++ test/gr/auth/ee/lcs/evaluators/RecallEvaluatorTest.java
@@ -1,28 +1,11 @@
-/*
- *	Copyright (C) 2011 by Allamanis Miltiadis
- *
- *	Permission is hereby granted, free of charge, to any person obtaining a copy
- *	of this software and associated documentation files (the "Software"), to deal
- *	in the Software without restriction, including without limitation the rights
- *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *	copies of the Software, and to permit persons to whom the Software is
- *	furnished to do so, subject to the following conditions:
- *
- *	The above copyright notice and this permission notice shall be included in
- *	all copies or substantial portions of the Software.
- *
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *	THE SOFTWARE.
+/**
+ * 
  */
 package gr.auth.ee.lcs.evaluators;
 
 import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import gr.auth.ee.lcs.AbstractLearningClassifierSystem;
 import gr.auth.ee.lcs.data.ClassifierTransformBridge;
 
@@ -32,12 +15,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Test the AccuracyEvaluator class.
- * 
  * @author Miltos Allamanis
  * 
  */
-public final class AccuracyEvaluatorTest extends EasyMockSupport {
+public class RecallEvaluatorTest extends EasyMockSupport {
 
 	AccuracyRecallEvaluator test;
 	AbstractLearningClassifierSystem mockLcs;
@@ -56,7 +37,7 @@ public final class AccuracyEvaluatorTest extends EasyMockSupport {
 				{ 0, 0, 1, 0, 0 }, { 1, 1, 1, 1, 1 } };
 		testSet = set;
 		test = new AccuracyRecallEvaluator(testSet, false, mockLcs,
-				AccuracyRecallEvaluator.TYPE_ACCURACY);
+				AccuracyRecallEvaluator.TYPE_RECALL);
 	}
 
 	/**
@@ -172,7 +153,7 @@ public final class AccuracyEvaluatorTest extends EasyMockSupport {
 				.anyTimes();
 
 		int[] resp2 = { 0 };
-		int[] resp2b = { 1 };
+		int[] resp2b = { 0, 1 };
 		expect(mockLcs.classifyInstance(testSet[2])).andReturn(resp2b)
 				.anyTimes();
 		expect(mockBridge.getDataInstanceLabels(testSet[2])).andReturn(resp2)
@@ -186,7 +167,7 @@ public final class AccuracyEvaluatorTest extends EasyMockSupport {
 				.anyTimes();
 
 		replayAll();
-		assertEquals(Double.compare(test.evaluateLCS(mockLcs), 1. / 6.), 0);
+		assertTrue(Math.abs(test.evaluateLCS(mockLcs) - (5. / 12.)) < 0.0001);
 		verifyAll();
 
 	}
